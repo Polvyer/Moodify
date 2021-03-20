0ed78225eae2eb3752a65bf2612aee9d441ebc6b
Polvyer <ocampowilliam27@gmail.com>
2021-03-20 01:58:48 -0500

    added example component and styled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Home from './components/Home';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: blue;
+  background-color: red;
+  padding: 10px;
+  border: 1px solid black;
+`;
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <p>Hello World!</p>
+      <Home text="Amy" />
+      <Home text="Jasmine" />
+      <Button>Click me!</Button>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <>
+      <div>Name: {props.text}</div>
+    </>
+  )
+}
+
+export default Home;
