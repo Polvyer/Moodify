d09ce5388867f62e48814235f37ac6346ab7fb95
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2021-03-21 05:55:31 -0500

    Updated GiphyAPI and constants file

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -11,6 +11,10 @@ const VISION_ENDPOINT = EXPRESS_BASE_URL + "/vision";
 /* Spotify API Endpoints */
 const SPOTIFY_SEARCH_ENDPOINT = "https://api.spotify.com/v1/search";
 
+/*GIPHY API KEY*/
+const APIKEY = "La98BpaXzKmJ61D8xAsleB5HU1IgTrX0"; 
+const GIPHY_SEARCH_ENDPOINT = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+
 /*
 const SPOTIFY_GET_FEATURED_PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/browse/featured-playlists";
 const SPOTIFY_SEVERAL_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/tracks';
@@ -28,4 +32,5 @@ export {
   SPOTIFY_ENDPOINT,
   VISION_ENDPOINT,
   SPOTIFY_SEARCH_ENDPOINT,
+  GIPHY_SEARCH_ENDPOINT
 };
